cd63ad6f36eb93e121d86721d14232af82a57ced
Pierre-Arthur DEMENGEL <124669857+pierrearthurdemengel@users.noreply.github.com>
2024-01-17 11:53:14 +0100

    Premier commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+app.get('/api/breachedaccount/:email', (req, res) => {
+  const email = req.params.email;
+  const API_KEY = 'MaCléPourAPI';
+
+  fetch(`https://haveibeenpwned.com/api/v3/breachedaccount/${encodeURIComponent(email)}`, {
+    headers: { 'hibp-api-key': API_KEY }
+  })
+  .then(apiRes => apiRes.json())
+  .then(data => res.json(data))
+  .catch(error => {
+    res.status(500).json({ error: error.toString() });
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
