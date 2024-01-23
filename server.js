bb35d848dc514c84e8fe2930ca80f8496b7ce38a
Pierre-Arthur DEMENGEL <124669857+pierrearthurdemengel@users.noreply.github.com>
2024-01-23 09:15:28 +0100

    Deuxième route pour permettre l'afficheage des détail des breachs

--- server.js
+++ server.js
@@ -19,5 +19,22 @@ app.get('/api/breachedaccount/:email', (req, res) => {
   });
 });
 
+app.get('/api/breach/:breachName', (req, res) => {
+  const breachName = req.params.breachName;
+  const API_KEY = process.env.API_KEY;
+
+  fetch(`https://haveibeenpwned.com/api/v3/breach/${breachName}`, {
+    headers: { 'hibp-api-key': API_KEY }
+  })
+  .then(apiRes => {
+    if (!apiRes.ok) throw new Error('Échec de la récupération des détails de la fuite.');
+    return apiRes.json();
+  })
+  .then(data => res.json(data))
+  .catch(error => {
+    res.status(500).json({ error: error.toString() });
+  });
+});
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
